10962f9ca0806354f1ac1e6c56491ddd5dee9b64
King Katraz <halnader89@gmail.com>
2021-01-14 02:14:57 -0500

    set up signup, login, logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   resources :genres
-  resources :users
+  resources :users, except: [:new] 
   resources :books
+
+  get '/signup', to: 'users#new'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
 end
